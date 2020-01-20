6c696ae9da73ee112bf9ee447c27ae0a65d71af7
yahya civelek <yahya.civelek@tr.panasonic.com>
2020-01-20 18:25:21 +0300

    simplify required headers

--- medium_query.py
+++ medium_query.py
@@ -1,7 +1,14 @@
+import os
 import requests
 import json
 import time
 import click
+
+headers = {
+    'x-xsrf-token': os.getenv('X_XSRF_TOKEN', 'any text!'),
+    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36',
+    'accept': 'application/json'
+}
 
 @click.group()
 def cli():
@@ -19,25 +26,7 @@ def generate_loop_index_list(start, stop, step=1):
 @click.option('-n', '--maxnum', default=9999, help='max. number of results. [10 - 9999)')
 @click.option('-o', '--output', default="results.json", help='Maximum number of results. Default: No restriction.')
 def query_medium(query, maxnum, output):
-  headers = {
-      'authority': 'medium.com',
-      'pragma': 'no-cache',
-      'cache-control': 'no-cache',
-      'x-client-date': '1578149049243',
-      'origin': 'https://medium.com',
-      'x-xsrf-token': '2HwkM3LR57ah',
-      'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36',
-      'content-type': 'application/json',
-      'accept': 'application/json',
-      'x-obvious-cid': 'web',
-      'sec-fetch-site': 'same-origin',
-      'sec-fetch-mode': 'cors',
-     #'referer': 'https://medium.com/search/posts?q=python',
-      'accept-encoding': 'gzip, deflate, br',
-      'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8,tr;q=0.7,ru;q=0.6',
-      #'cookie': '__cfduid=db577dfbf15d24836cc86858cf5e94aa21570375758; _ga=GA1.2.70317468.1570375760; _parsely_visitor={%22id%22:%22pid=5dc8a4e4fa4bd8d0797b9a85e107731e%22%2C%22session_count%22:1%2C%22last_session_ts%22:1570375761408}; lightstep_guid/lite-web=328bc18254245108; lightstep_session_id=6af920180687e670; lightstep_guid/medium-web=a8b100c4c2399456; pr=1; tz=-120; __cfruid=12c2fd853ce5aa32449c49b23eb67afef66daf93-1578111157; optimizelyEndUserId=e570a6b31af3; uid=e570a6b31af3; sid=1:VqJiCkQTSP0K8CikBXdclsnxkbJDeq8nTaTrc2dJ8az3POzmGWgbp6qM3ti7skFr; xsrf=2HwkM3LR57ah; sz=762',
-  }
-
+  print(os.getenv('X_XSRF_TOKEN', 'Token Not found'))
   params = (
       ('q', query),
   )
@@ -77,7 +66,7 @@ def query_medium(query, maxnum, output):
       next_page['pageSize'] = result_size
       data = json.dumps(next_page)
       #break
-      
+
   final_data = {
       "articles": article_list,
       "users": Users,
@@ -95,25 +84,6 @@ def query_medium(query, maxnum, output):
 #@click.option('-n', '--maxnum', default=9999, help='max. number of results. [10 - 9999)')
 @click.option('-o', '--output', default="archive.json", help='output file path')
 def collect_archive(tag, output):
-    headers = {
-        'authority': 'medium.com',
-        'pragma': 'no-cache',
-        'cache-control': 'no-cache',
-        'x-client-date': '1578149049243',
-        'origin': 'https://medium.com',
-        'x-xsrf-token': '2HwkM3LR57ah',
-        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36',
-        'content-type': 'application/json',
-        'accept': 'application/json',
-        'x-obvious-cid': 'web',
-        'sec-fetch-site': 'same-origin',
-        'sec-fetch-mode': 'cors',
-        #'referer': 'https://medium.com/search/posts?q=python',
-        'accept-encoding': 'gzip, deflate, br',
-        'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8,tr;q=0.7,ru;q=0.6',
-        #'cookie': '__cfduid=db577dfbf15d24836cc86858cf5e94aa21570375758; _ga=GA1.2.70317468.1570375760; _parsely_visitor={%22id%22:%22pid=5dc8a4e4fa4bd8d0797b9a85e107731e%22%2C%22session_count%22:1%2C%22last_session_ts%22:1570375761408}; lightstep_guid/lite-web=328bc18254245108; lightstep_session_id=6af920180687e670; lightstep_guid/medium-web=a8b100c4c2399456; pr=1; tz=-120; __cfruid=12c2fd853ce5aa32449c49b23eb67afef66daf93-1578111157; optimizelyEndUserId=e570a6b31af3; uid=e570a6b31af3; sid=1:VqJiCkQTSP0K8CikBXdclsnxkbJDeq8nTaTrc2dJ8az3POzmGWgbp6qM3ti7skFr; xsrf=2HwkM3LR57ah; sz=762',
-    }
-
     offset = len(b'])}while(1);</x>')
 
     def update_data(res):
